8dfdc2a5e2e673defa2a8a147a0be20e175ef685
David Fan <fand.kanade@gmail.com>
2019-12-18 00:36:32 -0500

    Added BlogPage API Endpoint

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,8 +1,14 @@
 from wagtail.api.v2.endpoints import BaseAPIEndpoint
-from blog.models import Recipe
+from blog.models import BlogPage, Recipe
 
 
 class RecipeAPIEndpoint(BaseAPIEndpoint):
     model = Recipe
     body_fields = BaseAPIEndpoint.body_fields + ['title', 'image', 'ingredients', 'instructions']
     listing_default_fields = BaseAPIEndpoint.listing_default_fields + ['title', 'image', 'ingredients', 'instructions']
+
+
+class BlogAPIEndpoint(BaseAPIEndpoint):
+    model = BlogPage
+    body_fields = BaseAPIEndpoint.body_fields + ['date', 'title', 'intro']
+    listing_default_fields = BaseAPIEndpoint.listing_default_fields + ['title', 'date', 'intro']
