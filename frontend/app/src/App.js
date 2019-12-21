b11e6d3ab13deee7afc21e166dc80d3cd1fee461
David Fan <fand.kanade@gmail.com>
2019-12-21 09:28:39 -0500

    Initial template integration

---
+++ frontend/app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import Index from './views/Index';
+
+//import Header from './components/Header/Header';
+//import Footer from './components/Footer/Footer';
+
+import './assets/css/bootstrap.min.css';
+import './assets/scss/paper-kit.scss';
+//import './assets/demo/demo.css';
+//import './assets/demo/react-demo.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" render={props => <Index {...props} />} />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
