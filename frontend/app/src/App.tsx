9a7dd2a7c66ec16b8696057f9003a6ecc9857a29
David Fan <fand.kanade@gmail.com>
2020-04-18 01:37:58 -0400

    Removed paper-kit

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,41 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-import Index from './views/Index';
-import BlogIndex from './views/BlogIndex';
-import BlogPost from './views/BlogPost';
-import NucleoIcons from './views/NucleoIcons';
-import Presentation from './views/Presentation';
-import Sections from './views/Sections';
+import Callie from './layout/view';
 
-import Callie from './callie/view';
-
-//import Header from './components/Header/Header';
-//import Footer from './components/Footer/Footer';
-
-//import './assets/css/Montserrat.css';
-import './assets/scss/fontawesome/scss/fontawesome.scss';
-import './assets/scss/fontawesome/scss/regular.scss';
-import './assets/scss/fontawesome/scss/solid.scss';
-import './assets/scss/fontawesome/scss/brands.scss';
-import './assets/css/bootstrap.min.css';
-import './assets/scss/paper-kit.scss';
-import './assets/demo/demo.css';
-import './assets/demo/react-demo.css';
+import './assets/callie/css/all.min.css';
+import './assets/callie/css/bootstrap.min.css';
+import './assets/callie/css/MontserratCallie.css';
+import './assets/callie/css/style.css';
 
 class App extends React.Component {
   render(){
     return (
       <BrowserRouter>
         <Switch>
+        {/*
           <Route path='/blog' component={BlogIndex} />
           <Route path='/post/:id' component={BlogPost} />
           <Route path='/home' component={Index} />
           <Route path='/icons' component={NucleoIcons} />
           <Route path='/presentation' component={Presentation} />
           <Route path='/sections' component={Sections} />
-          <Route path='/callie' component={Callie} />
           {/*<Route path="/" render={props => <Index {...props} />} />*/}
+          <Route path='/callie' component={Callie} />
           <Redirect to='/callie' />
         </Switch>
       </BrowserRouter>
