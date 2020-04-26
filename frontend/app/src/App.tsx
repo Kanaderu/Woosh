e04896e4c75c14223a1de9df0dcdb692fd268e40
David Fan <fand.kanade@gmail.com>
2020-04-26 15:24:09 -0400

    Updated supported languages and plugins for Prismjs

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-coy.css'
 
 import RegularView from './layout/RegularView';
 import ContactView from './layout/ContactView';
--- frontend/app/src/components/sections/content/CodeEntry.tsx
+++ frontend/app/src/components/sections/content/CodeEntry.tsx
@@ -1,6 +1,66 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Prism from 'prismjs';
-import 'prismjs/themes/prism.css'
+
+// list languages to highlight
+import 'prismjs/components/prism-markup-templating';
+
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cmake';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-django';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-git';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsonp';
+import 'prismjs/components/prism-kotlin';
+import 'prismjs/components/prism-latex';
+import 'prismjs/components/prism-lisp';
+import 'prismjs/components/prism-lua';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-makefile';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-matlab';
+import 'prismjs/components/prism-opencl';
+import 'prismjs/components/prism-perl';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-prolog';
+import 'prismjs/components/prism-rest';
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-tcl';
+import 'prismjs/components/prism-toml';
+import 'prismjs/components/prism-tsx';
+import 'prismjs/components/prism-verilog';
+import 'prismjs/components/prism-vhdl';
+import 'prismjs/components/prism-vim';
+import 'prismjs/components/prism-yaml';
+
+// list plugins to enable
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+
+import 'prismjs/plugins/toolbar/prism-toolbar.css';
+import 'prismjs/plugins/toolbar/prism-toolbar';
+
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+
+import 'prismjs/plugins/show-language/prism-show-language';
+
+import 'prismjs/plugins/match-braces/prism-match-braces.css';
+import 'prismjs/plugins/match-braces/prism-match-braces';
+
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
+
+//import 'prismjs/plugins/highlight-keywords/prism-highlight-keywords';
 
 export interface CodeEntryProps {
   language: string,
@@ -11,15 +71,16 @@ const CodeEntry: React.FC<CodeEntryProps> = ({language, code}) => {
   const codeElement = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
-    if (codeElement && codeElement.current) {
-      Prism.highlightElement(codeElement.current)
-    }
+    Prism.highlightAll()
+    // if (codeElement && codeElement.current) {
+    //   Prism.highlightElement(codeElement.current)
+    // }
   }, [code]);
 
   return (
     <div>
-      <pre>
-        <code ref={codeElement} className={`language-${language}`}>
+      <pre className="line-numbers">
+        <code ref={codeElement} className={"match-braces " + `language-${language}`}>
           {code}
         </code>
       </pre>
