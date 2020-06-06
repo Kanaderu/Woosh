f0f186699c58366a4d6393ef0b6608d0c4f3a838
David Fan <fand.kanade@gmail.com>
2020-06-06 04:54:49 -0400

    Updated Social widgets in footers

--- frontend/app/src/components/footers/widgets/FooterAboutWidget.tsx
+++ frontend/app/src/components/footers/widgets/FooterAboutWidget.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
+import SocialAPI, { platformToFontAwesome, platformToSocialColor } from '../../../types/SocialAPI';
+import useGetSocialSerivce from '../../../api/useGetSocialService';
+
 import altLogo from '../../../assets/logo/logo-text-w.png';
 
-const FooterAboutWidget: React.FC<{}> = () => {
+export interface FooterAboutWidgetProps  {
+  social?: SocialAPI[];
+}
+
+const FooterAboutWidget: React.FC<FooterAboutWidgetProps> = ({social}) => {
+  if(!social) {
+    const resultsSocial = useGetSocialSerivce();
+
+    if(resultsSocial.status == "loaded"){
+      social = resultsSocial.payload.items;
+      // console.log('Social:', social);
+    } else {
+      // handle state while loading
+    }
+  }
+
+  const renderSocial = social != undefined &&
+    <ul className="contact-social">
+    {
+      social.map((entry, key) => {
+        const icon = platformToFontAwesome(entry.platform);
+        const color = platformToSocialColor(entry.platform);
+        return <li key={key}><a href={entry.url} className={color}><i className={icon}></i></a></li>
+    })}</ul>;
+
   return (
     <>
       <div className="footer-widget">
@@ -10,12 +37,7 @@ const FooterAboutWidget: React.FC<{}> = () => {
           <a href="index.html" className="logo"><img src={altLogo} alt="" /></a>
         </div>
         <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
-        <ul className="contact-social">
-          <li><a href="#" className="social-facebook"><i className="fab fa-facebook"></i></a></li>
-          <li><a href="#" className="social-twitter"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="#" className="social-google-plus"><i className="fab fa-google-plus"></i></a></li>
-          <li><a href="#" className="social-instagram"><i className="fab fa-instagram"></i></a></li>
-        </ul>
+          {renderSocial}
       </div>
     </>
   );
